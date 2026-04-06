d0a5220a0b8f5b2a061e650bde349d27e04f9479
22ljz <2380189206@qq.com>
2026-04-06 04:36:51 +0000

    1

---
+++ main.py
@@ -0,0 +1,50 @@
+from telethon import TelegramClient, events
+from telethon.tl.types import DocumentAttributeFilename
+from telethon.sessions import StringSession
+import subprocess
+import os
+
+bot = TelegramClient(
+    StringSession(os.environ["TOKEN"]),
+    int(os.environ["API_ID"]),
+    os.environ["API_HASH"],
+)
+
+
+@bot.on(events.NewMessage(pattern="/start"))
+async def start(event):
+    """Send a message when the command /start is issued."""
+    await event.respond("Hi!")
+    raise events.StopPropagation
+
+
+@bot.on(events.NewMessage())
+async def download_file(event):
+    if event.file:
+        filename = None
+        for attr in event.message.media.document.attributes:
+            if isinstance(attr, DocumentAttributeFilename):
+                filename = attr.file_name
+                break
+
+        await event.reply(f"Downloading {filename}...")
+
+        await event.message.download_media(
+            file=filename,
+            progress_callback=lambda d, t: print(
+                f"Download: {d / t * 100:.1f}% ({d}/{t} bytes)", end="\r"
+            ),
+        )
+        await event.reply("File downloaded successfully!")
+        output = subprocess.check_output(["bash", "-c", f"./transfer wss {filename}"])
+        await event.reply(output)
+
+        bot.disconnect()
+
+
+def main():
+    bot.run_until_disconnected()
+
+
+if __name__ == "__main__":
+    main()
