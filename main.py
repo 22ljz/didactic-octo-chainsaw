40f1a16e907b50805e9712f8f6b532c48f0c86c3
22ljz <2380189206@qq.com>
2026-05-02 10:53:34 +0000

    1

--- main.py
+++ main.py
@@ -1,58 +1,64 @@
-from telethon import TelegramClient, events
-from telethon.tl.types import DocumentAttributeFilename
+from telethon import TelegramClient
 from telethon.sessions import StringSession
-import subprocess
 import os
+import asyncio
+import logging
+import boto3
+import time
 
-bot = TelegramClient(
+START = time.time()
+
+tg_client = TelegramClient(
     StringSession(os.environ["TOKEN"]),
     int(os.environ["API_ID"]),
     os.environ["API_HASH"],
-).start()
+)
+
+bucket = boto3.resource(
+    "s3",
+    aws_access_key_id=os.environ["ACCESS_ID"],
+    aws_secret_access_key=os.environ["ACCESS_SECRET"],
+    endpoint_url=os.environ["ENDPOINT_URL"],
+    region_name="",
+).Bucket("test")
 
 
-@bot.on(events.NewMessage(pattern="/start"))
-async def start(event):
-    """Send a message when the command /start is issued."""
-    await event.respond("Hi!")
-    raise events.StopPropagation
+async def upload_oss_file_to_tg(chat, oss_file_path):
+    file_name = os.path.basename(oss_file_path)
+    try:
+        bucket.download_file(oss_file_path, f"/tmp/{oss_file_path}")
 
-
-@bot.on(events.NewMessage())
-async def download_file(event):
-    if event.file:
-        filename = None
-        for attr in event.message.media.document.attributes:
-            if isinstance(attr, DocumentAttributeFilename):
-                filename = attr.file_name
-                break
-
-        msg = await event.reply(f"Downloading {filename}...")
-
-        await event.message.download_media(
-            file=filename,
-            progress_callback=lambda d, t: print(
-                f"Download: {d / t * 100:.1f}% ({d}/{t} bytes)", flush=True
-            ),
-        )
-        await msg.edit("File downloaded successfully!")
-        result = subprocess.run(
-            f"./transfer lit \"{filename}\"",
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            text=True,
-            shell=True,
-            encoding="utf-8"
+        await tg_client.send_file(
+            entity=chat,
+            file=f"/tmp/{oss_file_path}",
+            caption=file_name,
+            force_document=False,
+            supports_streaming=True,
+            file_name=file_name,
         )
 
-        await msg.edit(result.stdout.strip().splitlines()[-1].strip())
+        bucket.Object(oss_file_path).delete()
+        os.remove(f"/tmp/{oss_file_path}")
+        return
 
-        bot.disconnect()
+    except Exception as e:
+        logging.exception(e)
 
 
-def main():
-    bot.run_until_disconnected()
+async def scan_oss_folder_and_upload():
+    channel = await tg_client.get_entity(int(os.environ["TARGET"]))
+
+    for file_obj in bucket.objects.all():
+        if time.time() - START > 4 * 60 * 60:
+            return
+        await upload_oss_file_to_tg(channel, file_obj.key)
+    os.exit(-1)
+
+
+async def main():
+    async with tg_client:
+        await scan_oss_folder_and_upload()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
