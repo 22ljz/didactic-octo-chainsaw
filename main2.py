26727cdd5d010bf0f7b49af2e648acfe223e24d9
22ljz <2380189206@qq.com>
2026-06-20 02:29:46 +0000

    1

--- main2.py
+++ main2.py
@@ -29,9 +29,10 @@ async def main():
             if msg.text is not None and isinstance(msg.text, str):
                 message_list.append(msg)
         message_list = sorted(message_list, key=lambda msg: int(msg.text))
-        for msg in message_list:
-            logger.info(f"Processing {msg.text}...")
-            await msg.forward_to(channel2, drop_author=True)
+        for i in range(0, len(message_list), 100):
+            chunk = message_list[i : i + 100]
+            logger.info(f"Processing {len(chunk)}...")
+            await tg_client.send_message(channel2, chunk)
 
 
 if __name__ == "__main__":
