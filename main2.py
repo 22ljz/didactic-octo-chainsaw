97dac594f228706b9c28c7c49a9a3eac0fca3451
22ljz <2380189206@qq.com>
2026-06-20 03:14:10 +0000

    1

--- main2.py
+++ main2.py
@@ -13,7 +13,14 @@ logging.getLogger("telethon").setLevel(level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
+async def auto_exit():
+    await asyncio.sleep(5 * 3600)
+    await tg_client.disconnect()
+    exit(0)
+
+
 async def main():
+    asyncio.create_task(auto_exit)
     global tg_client
     tg_client = TelegramClient(
         StringSession(os.environ["TOKEN"]),
@@ -37,18 +44,17 @@ async def main():
                 and msg.text not in exist_set
             ):
                 message_list.append(msg)
+        if not message_list:
+            exit(-1)
         message_list = sorted(message_list, key=lambda msg: int(msg.text))
         for i in range(0, len(message_list), 100):
             chunk = message_list[i : i + 100]
             logger.info(f"Processing {len(chunk)}...")
-            await tg_client.forward_messages(
-                channel2, chunk, drop_author=True
-            )
+            await tg_client.forward_messages(channel2, chunk, drop_author=True)
 
 
 if __name__ == "__main__":
     try:
         asyncio.run(main())
-
-    except asyncio.CancelledError:
+    except:
         pass
